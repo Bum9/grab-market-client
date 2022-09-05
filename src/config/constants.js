5b4359cf6531f6aade4aa81462c41c0022e6652d
BUM <wkb999@naver.com>
2022-09-06 08:45:26 +0900

    api 안되는 거 수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://bum2-market-server.herokuapp.com/"
+    ? "https://bum2-market-server.herokuapp.com"
     : "http://localhost:8080";
